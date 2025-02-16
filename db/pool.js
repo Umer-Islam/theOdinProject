d06d134112d4b828dfce5d25d27f3662ea631256
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2025-02-16 17:30:39 +0500

    initial commit

--- db/pool.js
+++
@@ -1,6 +0,0 @@
-const { Pool } = require("pg");
-require('dotenv').config()
-// Again, this should be read from an environment variable
-module.exports = new Pool({
-  connectionString: process.env.DB_CONNECTION_STRING
-});
--- routes/signUpRouter.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express')
-const signUpRouter = Router()
-const {get,post} = require('../controllers/signUpController')
-
-signUpRouter.get('/',get)
-signUpRouter.post('/',post)
-
-module.exports = signUpRouter
